47a7d1b002ed235c168b77fa65d3a22bf257fac4
Tyler Chase <tylerchase@DN0a22b158.SUNet>
2017-03-12 16:44:58 -0700

    cleaned up code

--- code/classification_model/LSTM_classifier.py
+++ code/classification_model/LSTM_classifier.py
@@ -24,11 +24,11 @@ import pickle
 import random
 import time
 
-
 # Code recycled from hw3
 def pad_sequences(data, max_length):
     # Use this zero vector when padding sequences.
     # this value corresponds to '<end>' in the dictionary
+    # here we pad the end of the sentence
     zero_vector = 22
 
     X = data['x']
@@ -37,6 +37,25 @@ def pad_sequences(data, max_length):
         new_sentence = list(sentence)
         while len(new_sentence) < max_length:
             new_sentence.append(zero_vector)
+        X_2.append(new_sentence[:max_length])
+    data['x'] = np.array(X_2)
+    return data
+    
+def pad_sequences_2(data, max_length):
+    # Use this zero vector when padding sequences.
+    # this value corresponds to '<start>' in the dictionary
+    # here we pad the start of the sentence
+    zero_vector = 0
+
+    X = data['x']
+    X_2 = []
+    for sentence in X:
+        length = len(sentence)
+        length_dif = max_length - length
+        new_sentence = []
+        while len(new_sentence) < length_dif:
+            new_sentence.append(zero_vector)
+        new_sentence.extend(sentence)
         X_2.append(new_sentence[:max_length])
     data['x'] = np.array(X_2)
     return data
@@ -58,12 +77,15 @@ def convert_to_XY(data, word_dict):
     
     X = []
     Y = []
+    X_length = []
     data_2 = {}
     for subreddit, sentence in data:
         Y.append(sub_dict[subreddit])
         X.append([word_dict[j] for j in sentence])
+        X_length.append(len(sentence))
     data_2['x'] = np.array(X)
     data_2['y'] = np.array(Y)
+    data_2['x_length'] = np.array(X_length)
     return data_2
          
         
@@ -88,9 +110,9 @@ def import_dataset(address, word_dict, max_sentence_length, train_percent = 80, 
     dev = convert_to_XY(dev, word_dict)
     test = convert_to_XY(test, word_dict)
     # Pad sequences to make them the same max length
-    train = pad_sequences(train, max_sentence_length)
-    dev = pad_sequences(dev, max_sentence_length)
-    test = pad_sequences(test, max_sentence_length)
+    train = pad_sequences_2(train, max_sentence_length)
+    dev = pad_sequences_2(dev, max_sentence_length)
+    test = pad_sequences_2(test, max_sentence_length)
     return train, dev, test
 
     
@@ -116,15 +138,16 @@ def lazy_property(function):
     return wrapper
 
 
+# Class that stores important parameters
 class Config:
-    def __init__(self, sample_size, class_size, embed_length, max_sentence_length):
+    def __init__(self, sample_size, class_size, embed_length, max_sentence_length, layers):
         self.sample_size = sample_size
         self.class_size = class_size
         self.embed_length = embed_length
         self.max_sentence_length = max_sentence_length
-    
-    
-    
+        self.layers = layers    
+
+# LSTM classification model class
 class Classification:
     
     def __init__(self, config, embeddings, data, target, dropout, num_hidden = 200, learning_rate = 0.003):
@@ -144,17 +167,15 @@ class Classification:
         embed_input = tf.reshape(embed_input, [-1, self.config.max_sentence_length, self.config.embed_length]) 
         embed_input = tf.cast(embed_input, tf.float32)
         return(embed_input)
-        
-        
+       
     @lazy_property
     def prediction(self):
         # define LSTM cell
         lstm = tf.nn.rnn_cell.LSTMCell(self._num_hidden)
-        #lstm = tf.nn.rnn_cell.BasicLSTMCell(self._num_hidden)
         # Implement a dropout regularization on cell outputs 
         lstm = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob = self.dropout)
         # Link together (num_layers) number of cells to form the temporal model
-        lstm = tf.nn.rnn_cell.MultiRNNCell([lstm] * self.config.max_sentence_length)
+        lstm = tf.nn.rnn_cell.MultiRNNCell([lstm] * self.config.layers)
         # Create neaural network from cell
         output, _ = tf.nn.dynamic_rnn(lstm, self.embed_input, dtype = tf.float32)
         # Select last output for classification
@@ -174,7 +195,6 @@ class Classification:
         
     @lazy_property
     def optimize(self):
-        #opt = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
         opt = tf.train.GradientDescentOptimizer(self.learning_rate)
         return(opt.minimize(self.cost))
         
@@ -188,55 +208,11 @@ class Classification:
     @staticmethod
     def _initialize_weight_bias(input_length, output_length):
         # look at this closer
-        weight = tf.get_variable("weight_2", shape = [input_length, output_length],\
+        weight = tf.get_variable("weight", shape = [input_length, output_length],\
             initializer=tf.contrib.layers.xavier_initializer() )
-        bias = tf.get_variable("bias_2", shape = [output_length], \
+        bias = tf.get_variable("bias", shape = [output_length], \
             initializer=tf.constant_initializer(0) )
         return weight, bias
-
-
-        
-        
-'''      
-# Load embeddings
-# Data address
-address = r'/Users/tylerchase/Documents/Stanford_Classes/CS224n_Natural_Language_Processing_with_Deep_Learning/final project/data//'
-embeddings = np.load(address + 'large_weights.pkl_iter100')
-with open(address + 'embedding_dict') as input:
-    embedAddress_dict = pickle.load(input)
-    
-   
-# Model parameters
-epoch_size = 10
-minibatch_size = 100
-dropout_const = 0.5
-max_sentence_length = 20
-embed_length = 25
-    
-train, dev, test = import_dataset(address + '2015_data', embedAddress_dict, max_sentence_length)
-
-sample_size, class_size = np.shape(train['y'])
-data = tf.placeholder(tf.int32, [None, max_sentence_length, embed_length])
-target = tf.placeholder(tf.float32, [None, class_size])
-dropout = tf.placeholder(tf.float32)   
-embeddings = tf.constant(embeddings)
-embeddings = tf.nn.embedding_lookup(embeddings, data)
-embeddings = tf.reshape(embeddings, [-1, max_sentence_length, embed_length]) 
-print(embeddings.get_shape())
-embeddings = tf.cast(embeddings, tf.float32)
-model = classification(embeddings, target, dropout)
-sess = tf.Session()
-sess.run( tf.global_variables_initializer() )
-for epoch in xrange(epoch_size):
-    for i in np.arange(0, sample_size, minibatch_size):
-        batch_x = train['x'][i:i+minibatch_size]
-        batch_y = train['y'][i:i+minibatch_size]
-        sess.run(model.optimize, 
-                 {data:batch_x, target:batch_y, dropout:dropout_const} )
-    error = sess.run(model.error, 
-                     {data:test.data, target:test.target, dropout:1} )
-    print('Epoch:{:2d}), Error:{:3.1f}%'.format( (epoch + 1), (100*error))) 
-'''
         
 
 def main():
@@ -245,31 +221,34 @@ def main():
     # Data address
     address = r'/Users/tylerchase/Documents/Stanford_Classes/CS224n_Natural_Language_Processing_with_Deep_Learning/final project/data//'
     embeddings = np.load(address + 'large_weights.pkl_iter100')
+    zeroVecAdd, zeroVecLength = np.shape(embeddings)
     with open(address + 'embedding_dict') as input:
         embedAddress_dict = pickle.load(input)
       
     # Model parameters
-    epoch_size = 10
+    epoch_size = 15
     minibatch_size = 100
-    dropout_const = 0.5
+    dropout_const = 0.1875
     max_sentence_length = 20
+    layers = 2
 
     # Call function to import data
     # replace subreddits with one hot vectors
     # replace list of words with list of embedding matrix addresses
-    train, dev, test = import_dataset(address + '2015_data', embedAddress_dict, max_sentence_length)
+    train, dev, test = import_dataset(address + '2015_data', embedAddress_dict, 
+                                      max_sentence_length, train_percent = 80,
+                                      dev_percent = 10)
 
     sample_size, class_size = np.shape(train['y'])
     _, embed_length = np.shape(embeddings)
     batches = int(sample_size/minibatch_size)
-    config = Config(sample_size, class_size, embed_length, max_sentence_length)
+    config = Config(sample_size, class_size, embed_length, max_sentence_length, layers)
     data = tf.placeholder(tf.int32, [None, max_sentence_length])
     target = tf.placeholder(tf.float32, [None, class_size])
     dropout = tf.placeholder(tf.float32)   
     model = Classification(config, embeddings, data, target, dropout)
     sess = tf.Session()
     sess.run( tf.global_variables_initializer() )
-    #sess.run( tf.initialize_all_variables() )
     for epoch in xrange(epoch_size):
         start_epoch = time.clock()
         j = 0
@@ -280,19 +259,23 @@ def main():
             sess.run(model.optimize, 
                      {data:batch_x, target:batch_y, dropout:dropout_const} )
             if not j%100:
+                #loss = sess.run(model.cost, 
+                #     {data:train['x'], target:train['y'], dropout:1})
                 batch_time = time.clock() - start_batch
                 start_batch = time.clock()
                 print("Batch {:d}/{:d} of epoch {:d} finished in {:f} seconds".format(j, batches, (epoch+1), batch_time))
+                #print("Loss {:f}".format(loss))
             j+=1
         epoch_time = time.clock() - start_epoch
-        print("Epoch {:d} finished in {:f} seconds".format(epoch, epoch_time))
+        print("Epoch {:d} finished in {:f} seconds".format( (epoch + 1), epoch_time))
         error_test = sess.run(model.error, 
                      {data:test['x'], target:test['y'], dropout:1} )
         error_train = sess.run(model.error, 
-                     {data:train['x'], target:train['y'], dropout:1})
+                     {data:train['x'], target:train['y'], dropout:1} )
         print('Epoch:{:2d}, Training Error {:3.1f}%, Test Error:{:3.1f}%'.format( (epoch + 1), (100*error_train), (100*error_test)))
-        
-           
+        #print('Epoch:{:2d}, Training Error {:3.1f}%'.format( (epoch + 1), (100*error_train)))
+
+                  
 # Run main() if current namespace is main
 if __name__ == '__main__':
     main()
