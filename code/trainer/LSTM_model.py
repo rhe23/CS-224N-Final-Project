17157b393e1e40c048ff6900d9392771ece9d27b
WILL QIU <william.qiuu@gmail.com>
2017-03-12 18:30:59 -0700

    LSTM-Model-in-progress

--- code/trainer/LSTM_model.py
+++ code/trainer/LSTM_model.py
@@ -58,7 +58,7 @@ def get_masks(sentences, max_length):
 class Config:
 
     def __init__(self, max_length, embed_size, output_size, n_features =1 , n_classes=0, hidden_unit_size = 10, batch_size = 256, n_epochs = 10):
-
+        self.batch_size=96
         self.dev_set_size =0.1
         self.test_set_size = 0.1
         self.classify= False #determines if we're running a classification
@@ -73,11 +73,8 @@ class Config:
         self.output_size = output_size #the size of the vocab
         self.learning_rate = 0.05
 
-
 def generate_padded_seq(max_length, vocab_length, sentences):
-    return np.array([sentence + [vocab_length-1]*(max_length-len(sentence)) for sentence in sentences])
-
-
+    return np.array([sentence + [vocab_length-1]*(max_length-len(sentence)) for sentence in sentences], dtype=np.int32)
 
 class RNN_LSTM:
 
@@ -95,25 +92,28 @@ class RNN_LSTM:
 
         self.train_y, self.dev_y, self.test_y = y[train_inds],  y[dev_inds], y[test_inds]
 
+        self.vocab_by_inds = list(xrange(0, self.config.output_size))
+
         # #build model steps
         self.add_placeholders() #initiate placeholders
         self.pred = self.prediction() #forward prop
         self.loss = self.calc_loss(self.pred) #calculate loss
-        self.train = self.back_prop(self.loss) #optimization step
+        # self.train = self.back_prop(self.loss) #optimization step
+
 
     def add_placeholders(self):
 
-        self.input_placeholder = tf.placeholder(tf.int32, shape=(None, self.config.max_length)) #n_features should just be 1 here (the index into the embeddings)
+        self.input_placeholder = tf.placeholder(tf.int32, shape=(self.config.batch_size, None)) #n_features should just be 1 here (the index into the embeddings)
 
         self.dropout_placeholder = tf.placeholder(tf.float32)
 
-        self.mask_placeholder = tf.placeholder(tf.bool, [None, self.config.max_length])
+        self.mask_placeholder = tf.placeholder(tf.bool, [self.config.batch_size, self.config.max_length])
 
-        self.sequence_lengths = tf.placeholder(tf.int32, [None,])
+        self.labels_placeholder = tf.placeholder(tf.int32, shape= (self.config.batch_size, None))
 
-        self.labels_placeholder = tf.placeholder(tf.int32, shape= (None, self.config.max_length-1))
+        self.sequence_placeholder = tf.placeholder(tf.int32, [self.config.batch_size])
 
-    def create_feed_dict(self, inputs_batch, labels_batch=None, mask_batch = None, dropout = 1):
+    def create_feed_dict(self, inputs_batch, labels_batch=None, mask_batch = None, seq_length = None, dropout = 1):
 
         #dropout is the keep probability
         feed_dict = {self.input_placeholder : inputs_batch}
@@ -122,8 +122,10 @@ class RNN_LSTM:
             feed_dict[self.labels_placeholder] = labels_batch
         if self.config.drop_out:
             feed_dict[self.dropout_placeholder] = dropout
-        if mask_batch:
+        if mask_batch is not None:
             feed_dict[self.mask_placeholder] = mask_batch
+        if seq_length is not None:
+            feed_dict[self.sequence_placeholder] = seq_length
 
         return feed_dict
 
@@ -131,28 +133,23 @@ class RNN_LSTM:
         #take random sample of training set:
         inds = np.random.choice(self.training_set, math.floor(0.01*len(self.training_set)), replace=False)
 
-        inds_y = [i[1:] for i in inds]
         small_s = generate_padded_seq(self.config.max_length, self.config.output_size, inds)
-        small_s_y = generate_padded_seq(self.config.max_length-1, self.config.output_size, inds_y)
-        feed = self.create_feed_dict(inputs_batch= small_s, labels_batch= small_s_y, dropout= self.config.drop_out)
+        small_s_y = [i[1:] for i in small_s]
+        seq_len = [len(i) for i in inds]
+        masks = get_masks(inds, self.config.max_length)
 
+        feed = self.create_feed_dict(inputs_batch= small_s, labels_batch= small_s_y, dropout= self.config.drop_out, mask_batch=masks, seq_length = seq_len)
 
-        _, loss = session.run([self.train, self.loss], feed_dict=feed)
-        # print loss
-        pass
+        loss = session.run([self.loss], feed_dict=feed)
+        return loss
 
-
-
-    def add_embeddings(self): #use this function to convert inputs to a tensor of shape (none, sentence length (self.config.max_length), number of features * embedding size (self.config.n_features * self.config.embed_size)
+    def add_embeddings(self):
+        #use this function to convert inputs to a tensor of shape (none, sentence length (self.config.max_length), number of features * embedding size
+        # (self.config.n_features * self.config.embed_size)
         embeddings = tf.Variable(self.pretrain_embeddings, dtype = tf.float32)
         embeddings = tf.nn.embedding_lookup(embeddings, ids = self.input_placeholder)
         embeddings = tf.reshape(embeddings, [-1, self.config.max_length, self.config.n_features * self.config.embed_size])
         self.x = embeddings
-
-    def return_one_hot(self, ind):
-        zero = np.zeros(self.config.output_size)
-        zero[ind] = 0
-        return zero
 
     def set_cell(self):
         self.cell = tf.nn.rnn_cell.LSTMCell(num_units=self.config.hidden_unit_size, input_size=self.config.batch_size,
@@ -160,72 +157,67 @@ class RNN_LSTM:
         self.cell = tf.nn.rnn_cell.DropoutWrapper(cell = self.cell, output_keep_prob=self.dropout_placeholder)
         # self.cell = tf.nn.rnn_cell.OutputProjectionWrapper(cell=self.cell, output_size=self.config.output_size)
 
-
-    def train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch):
-
-        feed = self.create_feed_dict(inputs_batch, labels_batch=labels_batch, mask_batch=mask_batch,
-                                     dropout=self.config.dropout)
-
-        _, loss = sess.run([self.train_op, self.loss], feed_dict=feed)
-        return loss
-
-    def run_epoch(self, sess, parser, dev_set):
-
-        for i, inds in enumerate(get_batch(self.data_size, self.config.batch_size)): #inds are indices for the training example
-
-            batch = self.training_set[inds]
-
-
     def prediction(self): #main training function for the model
 
         #sets up the construction of the graphs such that when session is called these operations will run
+
         preds = []
         self.add_embeddings()
-        self.one_hot_index()
         self.set_cell()
-        state = self.cell.zero_state(self.config.batch_size, dtype = tf.float32) #initial state
+        # state = tf.Variable(self.cell.zero_state(self.config.batch_size, dtype = tf.float32), trainable=False) #initial state
 
-        outputs, states = tf.nn.dynamic_rnn(self.cell, self.x, sequence_length= self.sequence_lengths, initial_state=state, time_major=True)
+        outputs, states = tf.nn.dynamic_rnn(self.cell, inputs = self.x, dtype = tf.float32, sequence_length=self.sequence_placeholder)
+        outputs = tf.transpose(outputs, [1,0,2])
 
         W = tf.get_variable("W2", shape = [self.config.hidden_unit_size, self.config.output_size], initializer=tf.contrib.layers.xavier_initializer() )
         b = tf.get_variable("b2", shape = [self.config.output_size], initializer=tf.constant_initializer(0) )
 
-        for time_step in range(self.config.max_length-2):
+        for time_step in range(self.config.max_length):
             out = tf.gather(outputs, ( time_step ) )
             y_t = tf.nn.softmax( tf.matmul(out, W) + b )
             preds.append(y_t)
 
-
         preds = tf.pack(preds, axis=1)
-        return(preds)
+
+        return preds
 
     def calc_loss(self, preds):
         #calculate loss across every pair of words
+        # one_hot_mats = tf.one_hot(self.vocab_by_inds, depth = self.config.batch_size, axis = 0)
+        #
+        loss = 0
+        # print loss
+        for time_step in range(self.config.max_length-1):
+            loss += tf.nn.sparse_softmax_cross_entropy_with_logits(preds[:, time_step, :], self.labels_placeholder[:,time_step])
+            # one_hot = tf.transpose(tf.gather(one_hot_mats, self.labels_placeholder[:,time_step]))
+            # loss += -tf.reduce_sum(tf.matmul(tf.log(preds[:, time_step, :]), one_hot))
 
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(preds, self.labels_placeholder)
-        loss = tf.boolean_mask(loss, self.mask_placeholder)
-        loss = tf.reduce_mean(loss)
+        # loss = tf.nn.sparse_softmax_cross_entropy_with_logits(preds, self.labels_placeholder)
+        # loss = tf.boolean_mask(loss, self.mask_placeholder)
+        # loss = tf.reduce_mean(loss)
 
+        # return loss
         return loss
+
     def back_prop(self, loss):
 
         optimizer = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate)
         train_op = optimizer.minimize(loss)
 
         return train_op
-
-    def fit(self, sess, saver, train_examples, dev_set):
-
-        best_dev_UAS = 0
-        for epoch in range(self.config.n_epochs):
-            print "Epoch {:} out of {:}".format(epoch + 1, self.config.n_epochs)
-            dev_UAS = self.run_epoch(sess, parser, train_examples, dev_set)
-            if dev_UAS > best_dev_UAS:
-                best_dev_UAS = dev_UAS
-                if saver:
-                    print "New best dev UAS! Saving model in ./data/weights/parser.weights"
-                    saver.save(sess, './data/weights/parser.weights')
-            print
+    #
+    # def fit(self, sess, saver, train_examples, dev_set):
+    #
+    #     best_dev_UAS = 0
+    #     for epoch in range(self.config.n_epochs):
+    #         print "Epoch {:} out of {:}".format(epoch + 1, self.config.n_epochs)
+    #         dev_UAS = self.run_epoch(sess, parser, train_examples, dev_set)
+    #         if dev_UAS > best_dev_UAS:
+    #             best_dev_UAS = dev_UAS
+    #             if saver:
+    #                 print "New best dev UAS! Saving model in ./data/weights/parser.weights"
+    #                 saver.save(sess, './data/weights/parser.weights')
+    #         print
 
 embeddings = get_embeddings()
 embeddings = np.vstack([embeddings, np.zeros(embeddings.shape[1])])
@@ -248,12 +240,11 @@ subsample_x = [get_indices(j) for j in all_dat['personalfinance']]
 subsample_y = [get_indices(j) for j[1:] in all_dat['personalfinance']]
 max_length = max(len(i) for i in subsample_x)
 
-
 #seq_length, max_length, embed_size, output_size
-c = Config(max_length = max_length, embed_size = embeddings.shape[1], output_size=embeddings.shape[0])
+c = Config(max_length = max_length, embed_size = embeddings.shape[1], output_size=embeddings.shape[0], batch_size = 96)
 m = RNN_LSTM(embeddings = embeddings, x= np.array(subsample_x), y = np.array(subsample_y), config= c, data_size=len(subsample_x))
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
-    m.test_session(sess)
-
+    loss = m.test_session(sess)
+    print loss
