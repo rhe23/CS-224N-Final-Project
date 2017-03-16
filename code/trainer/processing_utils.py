2772feb85d6c3998ce1aad237bd4cc73952fe94a
WILL QIU <william.qiuu@gmail.com>
2017-03-15 23:09:49 -0700

    split up util functions and implemented get predicted labels, also implemented argparse commands

---
+++ code/trainer/processing_utils.py
@@ -0,0 +1,42 @@
+import cPickle, math
+import numpy as np
+
+def get_embeddings(embed_path = './data/new_embeddings.pkl'):
+
+    with open(embed_path, 'rb') as f:
+        embeddings = cPickle.load(f)
+        f.close()
+    return embeddings
+
+def get_data(path):
+
+    with open(path, 'rb') as f:
+        input = cPickle.load(f)
+        f.close()
+
+    return input
+
+def get_batch(data_size, batch_size, shuffle=True):
+#returns a list of indices for each batch of data during optimization
+    data_indices = np.arange(data_size)
+    np.random.shuffle(data_indices)
+
+    for i in xrange(0, data_size, batch_size):
+       yield data_indices[i:i+batch_size]
+
+def get_dev_test_sets(dev_size, test_size, training_indices):
+    #dev_size should be a float between 0.0 and 1.0
+    #returns a list of indices for both training and dev sets
+
+    total_sizes = dev_size+test_size
+    temp_inds = np.random.choice(training_indices, int(math.floor(total_sizes*len(training_indices))), replace = False)
+    training_inds = [i for i in training_indices if i not in temp_inds]
+    dev_inds = temp_inds[:len(temp_inds)/2]
+    test_inds = temp_inds[len(temp_inds)/2:]
+
+    return (training_inds, dev_inds, test_inds)
+
+
+def get_masks(sentences, max_length):
+    return np.array([len(sentence)*[True] + (max_length-len(sentence))*[False] for sentence in sentences])
+
