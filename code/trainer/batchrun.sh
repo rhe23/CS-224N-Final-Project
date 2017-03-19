de131e00bd9437be927bc91d1d61b4b11a361281
WILL QIU <william.qiuu@gmail.com>
2017-03-19 09:32:01 -0700

    ...

--- code/trainer/batchrun.sh
+++ code/trainer/batchrun.sh
@@ -4,7 +4,7 @@ for subreddit in 'AskReddit' 'LifeProTips' 'nottheonion' 'news' 'science' 'trees
 
 do
 	echo "$subreddit"
-	python LSTM_model_single.py train -r "$subreddit" -lr 0.0003 -hs 200 -do 0.9 -l 1
+	python LSTM_model_single.py train -r "$subreddit" -lr 0.0005 -hs 200 -do 0.9 -l 1
 
 done
 
