e2e3c6fc4f5b1643ad80a10d93888f2e117f3e4f
WILL QIU <william.qiuu@gmail.com>
2017-03-18 19:38:34 -0700

    bug

--- code/trainer/batchrun.sh
+++ code/trainer/batchrun.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for subreddit in 'AskReddit' 'LifeProTips' 'nottheonion' 'news' 'science' 'trees' 'tifu' 'personalfinance' 'mildlyinteresting' 'interestingasfuck'
+for subreddit in 'news' 'science' 'trees' 'tifu' 'personalfinance' 'mildlyinteresting' 'interestingasfuck'
 
 do
+	echo "$subreddit"
 	python LSTM_model_single.py train -r "$subreddit" -lr 0.0004 -hs 100 -do 1
 
 done
