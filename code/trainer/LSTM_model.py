9aeb4353f6ba924903ff0701fac952e45b1a530d
WILL QIU <william.qiuu@gmail.com>
2017-03-19 15:35:11 -0700

    try variable lengths

--- code/trainer/LSTM_model.py
+++ code/trainer/LSTM_model.py
@@ -281,10 +281,10 @@ def train(args):
     r = args.subreddit
     sample = np.array([get_indices(j) for j in all_dat[r]])
     # subsample_y = [get_indices(j) for j[1:] in all_dat['personalfinance']][0:100]
-    max_length = max(len(i) for i in sample)
+    max_length = 10
 
     #seq_length, max_length, embed_size, output_size
-    config_file = Config(max_length = max_length, embed_size = embeddings.shape[1], output_size=embeddings.shape[0], batch_size = 36, drop_out=args.dropout, learning_rate = args.learningrate, hidden_unit_size=args.hiddensize)
+    config_file = Config(max_length = max_length, embed_size = embeddings.shape[1], output_size=embeddings.shape[0], batch_size = 128, drop_out=args.dropout, learning_rate = args.learningrate, hidden_unit_size=args.hiddensize)
 
     idx = np.arange(len(sample))
 
