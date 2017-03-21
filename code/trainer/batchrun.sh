1bd5374128933e18e2def93453549b1d189aad38
WILL QIU <william.qiuu@gmail.com>
2017-03-20 20:11:27 -0700

    b

--- code/trainer/batchrun.sh
+++ code/trainer/batchrun.sh
@@ -4,7 +4,7 @@ for subreddit in 'AskReddit' 'LifeProTips' 'nottheonion' 'news' 'science' 'trees
 
 do
 	echo "$subreddit"
-	python LSTM_model_variable_lengths.py train -r "$subreddit" -lr 0.0004 -hs 200 -do 1 -l 1 -sq 3 -p 0
+	python LSTM_model_variable_lengths.py train -r "$subreddit" -lr 0.0004 -hs 200 -do 1 -l 1 -sq 2 -p 0
 
 done
 
