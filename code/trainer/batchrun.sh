3780b348dd69f628ef50d7ff280ad809dc0bca8f
WILL QIU <william.qiuu@gmail.com>
2017-03-20 17:09:35 -0700

    asda

--- code/trainer/batchrun.sh
+++ code/trainer/batchrun.sh
@@ -4,16 +4,7 @@ for subreddit in 'AskReddit' 'LifeProTips' 'nottheonion' 'news' 'science' 'trees
 
 do
 	echo "$subreddit"
-	python LSTM_model_variable_lengths.py train -r "$subreddit" -lr 0.0004 -hs 200 -do 1 -l 1 -sq 3 -p 0
-
-done
-
-
-for subreddit in 'AskReddit' 'LifeProTips' 'nottheonion' 'news' 'science' 'trees' 'tifu' 'personalfinance' 'mildlyinteresting' 'interestingasfuck'
-
-do
-	echo "$subreddit"
-	python LSTM_model_variable_lengths.py generate -g "$subreddit" -nw 15 -n 100 -l 1 -sq 3 -hs 200 -p 0
+	python LSTM_model_variable_lengths.py train -r "$subreddit" -lr 0.0004 -hs 200 -do 1 -l 1 -sq 2 -p 0
 
 done
 
